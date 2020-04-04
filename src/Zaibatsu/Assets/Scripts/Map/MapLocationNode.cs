a1581dfdcef7520156983c47d17599fcbb10f80c
SilasReinagel <silas.reinagel@gmail.com>
2020-04-04 03:13:28 -0700

    Display Map Location Description on Hover. Closes #16

--- src/Zaibatsu/Assets/Scripts/Map/MapLocationNode.cs
+++ src/Zaibatsu/Assets/Scripts/Map/MapLocationNode.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public sealed class MapLocationNode : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI nameLabel;
+    [SerializeField] private TextMeshProUGUI descriptionLabel;
 
     public void Init(Location l)
     {
         nameLabel.text = l.DisplayName;
+        descriptionLabel.text = l.Description;
     }
 }
