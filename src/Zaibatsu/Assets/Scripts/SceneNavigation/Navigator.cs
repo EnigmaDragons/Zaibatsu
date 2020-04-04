b824c22e17e87519cb7787aa0217f92f41702f40
SilasReinagel <silas.reinagel@gmail.com>
2020-04-03 21:28:40 -0700

    Added Navigator. Can Start Game. Closes #13

---
+++ src/Zaibatsu/Assets/Scripts/SceneNavigation/Navigator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public sealed class Navigator : ScriptableObject
+{
+    private string _currentScene;
+    private string _previousScene;
+
+    [SerializeField] private bool loadSynchronously;
+
+    public void NavigateToMainMenu() => NavigateTo("MainMenu");
+    public void NavigateToGameScene() => NavigateTo("GameScene");
+    public void NavigateBack() => NavigateTo(_previousScene);
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    
+    private void NavigateTo(string name)
+    {
+        _previousScene = SceneManager.GetActiveScene().name;
+        _currentScene = name;
+        
+        // TODO: This should eventually be injected as an OnNavigate action instead.
+        PlayerPrefs.Save();
+        
+        if (!loadSynchronously)
+        {
+            var loading = SceneManager.LoadSceneAsync(name);
+            if (LoadingScreen.Instance != null)
+                LoadingScreen.Instance.Init(loading);
+        }
+        else
+        {
+            SceneManager.LoadScene(name);
+        }
+    }
+}
---
+++ src/Zaibatsu/Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button startGameButton;
+    [SerializeField] private Navigator navigator;
+
+    private void Awake()
+    {
+        startGameButton.onClick.AddListener(() => navigator.NavigateToGameScene());
+    }
+}
