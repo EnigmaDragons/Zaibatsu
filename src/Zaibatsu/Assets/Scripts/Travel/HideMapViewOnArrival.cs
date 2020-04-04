b39ee4fe1d6bcdb5765ed8fcf35a9c167a9bc1bf
SilasReinagel <silas.reinagel@gmail.com>
2020-04-04 16:01:11 -0700

    Show new location after arrival. Closes #49

---
+++ src/Zaibatsu/Assets/Scripts/Travel/HideMapViewOnArrival.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public sealed class HideMapViewOnArrival : OnMessage<ArrivedAtLocation>
+{
+    [SerializeField] private float delayBeforeHiding = 1f;
+
+    protected override void Execute(ArrivedAtLocation msg) => StartCoroutine(HideAfterDelay());
+
+    private IEnumerator HideAfterDelay()
+    {
+        yield return new WaitForSeconds(delayBeforeHiding);
+        Message.Publish(new ChangeUiElementStateRequested(GameUiElement.MapView, UiCommand.Hide));
+    }
+}
