58e2c2bb5be25af505a4817f187015b5cf56cd5d
SilasReinagel <silas.reinagel@gmail.com>
2020-04-04 13:07:53 -0700

    Location 2D spawning happens. Closes #45

--- src/Zaibatsu/Assets/Scripts/Locations/Location.cs
+++ src/Zaibatsu/Assets/Scripts/Locations/Location.cs
@@ -6,8 +6,10 @@ public sealed class Location : ScriptableObject
     [SerializeField] private string displayName;
     [SerializeField] private string description;
     [SerializeField] private Vector2 geoPosition;
+    [SerializeField] private GameObject locationPrefab;
 
     public string DisplayName => displayName;
     public string Description => description;
     public Vector2 GeoPosition => geoPosition;
+    public GameObject LocationPrefab => locationPrefab;
 }
---
+++ src/Zaibatsu/Assets/Scripts/Locations/Location2dSpawner.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location2dSpawner : OnMessage<BeganTravellingToLocation>
+{
+    [SerializeField] private CurrentGameMap map;
+
+    private readonly Dictionary<string, GameObject> _locations = new Dictionary<string, GameObject>();
+    
+    private void Start() => Spawn(map.CurrentLocation);
+
+    protected override void Execute(BeganTravellingToLocation msg) => Spawn(msg.Destination);
+
+    private void Spawn(Location location)
+    {
+        foreach (var l in _locations) 
+            l.Value.SetActive(false);
+        
+        if (_locations.TryGetValue(location.DisplayName, out var cached))
+            cached.SetActive(true);
+        else
+            _locations[location.DisplayName] = Instantiate(location.LocationPrefab, transform);
+    }
+}
