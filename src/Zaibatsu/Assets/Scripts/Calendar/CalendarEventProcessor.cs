07c689b43e3e599d544ef91780f29b0672dea8e1
SilasReinagel <silas.reinagel@gmail.com>
2020-04-05 13:40:38 -0700

    Events can be marked as Recurring. Closes #118

---
+++ src/Zaibatsu/Assets/Scripts/Calendar/CalendarEventProcessor.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+// TODO Implement this logic in #100
+public class CalendarEventProcessor : OnMessage<GameStateChanged, ArrivedAtLocation, SequenceStateChanged>
+{
+    [SerializeField] private CurrentGameCalendar calendar;
+    [SerializeField] private CurrentGameState game;
+    [SerializeField] private CurrentSequence sequence;
+
+    protected override void Execute(GameStateChanged msg)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Execute(ArrivedAtLocation msg) 
+        => calendar
+            .LocalEventFor(msg.Location, game.State.Time)
+            .IfPresent(e => sequence.StartSequence(e.SequenceName));
+
+    protected override void Execute(SequenceStateChanged msg)
+    {
+        throw new System.NotImplementedException();
+    }
+}
