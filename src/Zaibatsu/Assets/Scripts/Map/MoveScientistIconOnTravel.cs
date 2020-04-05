f8b1957570ec690d74fd87f7fcad25da1ff432e3
SilasReinagel <silas.reinagel@gmail.com>
2020-04-05 00:34:35 -0700

    Map improvements and fix. Closes #40. Closes #98

--- src/Zaibatsu/Assets/Scripts/Map/MoveScientistIconOnTravel.cs
+++ src/Zaibatsu/Assets/Scripts/Map/MoveScientistIconOnTravel.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 [RequireComponent(typeof(RectTransform))]
 public class MoveScientistIconOnTravel : OnMessage<BeganTravellingToLocation, ArrivedAtLocation>
 {
+    [SerializeField] private CurrentLocation current;
+    [SerializeField] private Floating floating;
+    
     private RectTransform _t;
 
     private void Awake() => _t = GetComponent<RectTransform>();
+    private void OnEnable() => MoveTo(current.Location);
     
-    protected override void Execute(BeganTravellingToLocation msg)
+    protected override void Execute(BeganTravellingToLocation msg) {}
+    protected override void Execute(ArrivedAtLocation msg) => MoveTo(msg.Location);
+    
+    private void MoveTo(Location l)
     {
+        floating.enabled = false;
+        _t.anchoredPosition = l.GeoPosition;
+        floating.enabled = true;
     }
-
-    protected override void Execute(ArrivedAtLocation msg) => _t.anchoredPosition = msg.Location.GeoPosition;
 }
