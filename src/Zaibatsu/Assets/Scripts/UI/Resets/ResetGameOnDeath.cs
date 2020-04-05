cbaf30e4529df535301045a2a3c7ede10dcefc8e
SilasReinagel <silas.reinagel@gmail.com>
2020-04-04 21:25:14 -0700

    Improved UI of Story Picture mode and Death. Closes #93

--- src/Zaibatsu/Assets/Scripts/UI/Resets/ResetGameOnDeath.cs
+++ src/Zaibatsu/Assets/Scripts/UI/Resets/ResetGameOnDeath.cs
@@ -7,7 +7,8 @@ public class ResetGameOnDeath : OnMessage<KillPlayer>
     [SerializeField] private FloatReference delayBeforeReset;
 
     protected override void Execute(KillPlayer msg) => StartCoroutine(Reset());
-
+    public void Execute() => StartCoroutine(Reset()); 
+    
     private IEnumerator Reset()
     {
         Message.Publish(new ChangeUiElementStateRequested(GameUiElement.DiedView, UiCommand.Show));
