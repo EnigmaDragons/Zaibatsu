cc0968123672e0eecf19a224e1b9b52f9cf806a5
SilasReinagel <silas.reinagel@gmail.com>
2020-04-05 16:15:12 -0700

    Shows Choices to Player. Closes #123

--- src/Zaibatsu/Assets/Scripts/UI/Story/ChoiceSelectionView.cs
+++ src/Zaibatsu/Assets/Scripts/UI/Story/ChoiceSelectionView.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class ChoiceSelectionView : MonoBehaviour
 {
     [SerializeField, AssetsOnly] private ChoiceButton buttonPrototype;
+    [SerializeField] private GameObject buttonParent;
     [SerializeField] private CurrentSequence seq;
     
     private readonly ChoiceButton[] _buttons = new ChoiceButton[8];
+
+    private List<Choice> _choices;
     
     public void SetChoices(List<Choice> choices)
     {
+        _choices = choices;
         for (var i = 0; i < 8; i++)
         {
             if (_buttons[i] == null)
-                _buttons[i] = Instantiate(buttonPrototype, transform);
+                _buttons[i] = Instantiate(buttonPrototype, buttonParent.transform);
             if (i < choices.Count)
-                _buttons[i].gameObject.SetActive(false);
-            else
             {
-                var choice = choices[i];
-                _buttons[i].Init(choice.Text, () => seq.PickChoice(choice.NextID));
+                var choiceIndex = i;
+                _buttons[i].Init($"{i + 1}. {choices[i].Text}", () => SelectChoice(choiceIndex));
             }
+            else
+                _buttons[i].gameObject.SetActive(false);
         }
+    }
+
+    public void SelectChoice(int index)
+    {
+        gameObject.SetActive(false);
+        seq.PickChoice(_choices[index].NextID);
     }
 }
