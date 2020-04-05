5a256a8a768684b8d2c9aa8cbc0bcffa3c5ad64c
SilasReinagel <silas.reinagel@gmail.com>
2020-04-04 23:23:59 -0700

    Designed basic Calendar. Closes #54

---
+++ src/Zaibatsu/Assets/Scripts/Calendar/CalendarEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+[Serializable]
+public sealed class CalendarEvent
+{
+    public Location Location { get; set; }
+    public string Description { get; set; }
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
+    public string SequenceName { get; set; }
+    
+    public override string ToString() => $"{Location.DisplayName}: {StartTime}-{EndTime} - {Description}";
+
+    public bool IsActiveAt(GameTime time) 
+        => time.TotalMinutes >= GameTime.Parse(StartTime).TotalMinutes 
+        && time.TotalMinutes < GameTime.Parse(EndTime).TotalMinutes;
+}
---
+++ src/Zaibatsu/Assets/Scripts/Calendar/StaticCalendar.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StaticCalendar : ScriptableObject
+{
+    [SerializeField] private CalendarEventAsset[] eventAssets;
+    [SerializeField] private CalendarEvent[] events;
+
+    public CalendarEvent[] AllEvents 
+        => eventAssets.Select(e => e.AsEvent())
+            .Concat(events ?? Enumerable.Empty<CalendarEvent>())
+            .ToArray();
+}
