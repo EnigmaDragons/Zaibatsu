46e9f40cf350ae4a28bc6012bfd83179b7b17b7c
SilasReinagel <silas.reinagel@gmail.com>
2020-04-05 15:56:03 -0700

    Create Choice View. Closes #124

---
+++ src/Zaibatsu/Assets/Scripts/UI/Story/ChoiceSelectionView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class ChoiceSelectionView : MonoBehaviour
+{
+    [SerializeField, AssetsOnly] private ChoiceButton buttonPrototype;
+    [SerializeField] private CurrentSequence seq;
+    
+    private readonly ChoiceButton[] _buttons = new ChoiceButton[8];
+    
+    public void SetChoices(List<Choice> choices)
+    {
+        for (var i = 0; i < 8; i++)
+        {
+            if (_buttons[i] == null)
+                _buttons[i] = Instantiate(buttonPrototype, transform);
+            if (i < choices.Count)
+                _buttons[i].gameObject.SetActive(false);
+            else
+            {
+                var choice = choices[i];
+                _buttons[i].Init(choice.Text, () => seq.PickChoice(choice.NextID));
+            }
+        }
+    }
+}
