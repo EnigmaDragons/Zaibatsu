8f1612c1cbc602f74a4209fd61ae7ba33f046033
SilasReinagel <silas.reinagel@gmail.com>
2020-04-04 22:24:02 -0700

    Added Corridors to Silvos Lab. Closes #85. Closes #95

---
+++ src/Zaibatsu/Assets/Scripts/UI/Story/StartSequenceOnClicked.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class StartSequenceOnClicked : MonoBehaviour
+{
+    [SerializeField] private CurrentSequence seq;
+    [SerializeField] private TextAsset asset;
+
+    public void Awake() 
+        => GetComponent<Button>().onClick.AddListener(
+            () => seq.ChangeSequence(asset.name));
+}
