3b5b3197ce9a1b092610d3f435cb69cf61d8e4c6
SilasReinagel <silas.reinagel@gmail.com>
2020-04-05 12:36:44 -0700

    Displays Thoughts. Closes #111

---
+++ src/Zaibatsu/Assets/Scripts/Events/Processing/DisplayThought.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DisplayThought : OnMessage<ThinkToSelf>
+{
+    [SerializeField] private ThoughtView view;
+
+    protected override void Execute(ThinkToSelf msg)
+    {
+        view.Set(msg.Thought);
+        view.gameObject.SetActive(true);
+    }
+}
