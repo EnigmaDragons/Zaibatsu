6d44b321ba6cca2649c1bd2ce65a27ce53a566e4
SilasReinagel <silas.reinagel@gmail.com>
2020-04-05 12:31:10 -0700

    Create Thought View. Closes #116

---
+++ s-commit.bat
@@ -0,0 +1,5 @@
+@echo off
+set arg1=%1
+git checkout src/Zaibatsu/ProjectSettings/QualitySettings.asset
+git add .
+git commit -m %arg1%
---
+++ src/Zaibatsu/Assets/Scripts/UI/Story/ThoughtView.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThoughtView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Button dismiss;
+
+    private void Awake() => dismiss.onClick.AddListener(Dismiss);
+    
+    public void Set(string txt)
+    {
+        text.text = txt;
+    }
+
+    public void Dismiss()
+    {
+        gameObject.SetActive(false);
+        Message.Publish(new SequenceStepFinished());
+    }
+}
